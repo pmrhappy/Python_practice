b444792173697bb409c7d67cbd891c7c834e5fb0
Mush Hsieh <MushHsieh@itri.org.tw>
2017-01-23 13:36:44 +0800

    refactor thread_vs_process

--- thread_vs_process/thread_vs_process.py
+++ thread_vs_process/thread_vs_process.py
@@ -7,33 +7,23 @@ def func(arg):
     time.sleep(3)
     return arg
 
-def evoke_thread(function, **func_kwargs):
-    thread = Thread(target=function, kwargs=func_kwargs)
-    thread.start()
+def evoke_procedure(Procedure, function, **func_kwargs):
+    procedure = Procedure(target=function, kwargs=func_kwargs)
+    procedure.start()
 
-def evoke_process(function, *func_args):
-    process = Process(target=function, args=func_args)
-    process.start()
-
-def test_thread(data):
-    time_thread_start = time.time()
-    evoke_thread(func, arg=data)
-    time_thread_end = time.time()
-    print("thread time:     ", time_thread_end - time_thread_start)
-
-def test_process(data):    
-    time_process_start = time.time()
-    evoke_process(func, data)
-    time_process_end = time.time()
-    print("process time:    ", time_process_end - time_process_start)
+def test(data, Procedure, type):
+    time_start = time.time()
+    evoke_procedure(Procedure, func, arg=data)
+    time_end = time.time()
+    print(type, " time:     ", time_end - time_start)
     
 if __name__ == '__main__':     
     data = [i for i in range(100)]
-    test_thread(data)
-    test_process(data)
-    
-    data = [i for i in range(10000000)]
-    test_thread(data)
-    test_process(data)
+    test(data, Thread, 'Thread')
+    test(data, Process, 'Process')
+
+    data = [i for i in range(100000)]
+    test(data, Thread, 'Thread')
+    test(data, Process, 'Process')
 
     
